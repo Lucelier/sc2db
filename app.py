b8d177d6e4347d099a7ba91c1ad1a4993783cb16
Lucelier <luc-rossier@bluewin.ch>
2023-11-16 17:02:53 +0100

    index inklusive API abfrage mit Tabellarischer-Darstellungen erstellt

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from main import get_database_connection, get_valid_rasse, query_database
 from rasse_enum import Rasse, rasse_mapping
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', data=None)
+
 @app.route('/abfrage', methods=['GET'])
 def abfrage():
     rasse_input = request.args.get('rasse')
 
-    print(f"Received rasse_input: {rasse_input}")
-
     connection = get_database_connection()
     selected_rasse = get_valid_rasse(rasse_input, rasse_mapping)
-
-    print(f"Selected Rasse: {selected_rasse}")
 
     if selected_rasse is not None:
         if selected_rasse == Rasse.Terraner:
@@ -25,23 +25,12 @@ def abfrage():
 
         results = query_database(connection, rasse)
 
-        # Korrigierte Reihenfolge der Spaltennamen
-        column_names = [
-            "ID", "Name", "Kosten Mineralien", "Kosten Vespingas",
-            "Sonstiges", "Rasse", "Versorgung", "Bauzeit",
-            "Ausbildungsort", "Trefferpunkte", "Energie", "Panzerung",
-            "Begrenzte Lebensdauer", "Typ"
-        ]
-
-        # Extrahiere die relevanten Informationen aus den Ergebnissen
         if results is not None:
+            column_names = ["ID", "Name", "Kosten_Mineralien", "Kosten_Vespingas", "Sonstiges", "Rasse", "Versorgung", "Bauzeit", "Ausbildungsort", "Trefferpunkte", "Energie", "Panzerung", "Begrenzte_Lebensdauer", "Typ"]
             extracted_data = [dict(zip(column_names, row)) for row in results]
-        else:
-            extracted_data = []
+            return render_template('index.html', data=extracted_data)
 
-        return jsonify({"data": extracted_data})
-    else:
-        return jsonify({"message": f"Ungültige Rasse. rasse_input: {rasse_input}"})
+    return render_template('index.html', data=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
